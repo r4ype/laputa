88bce8e7b27065b8dd5ba69e157f001c29fef02d
r4ype <raypemaster@gmail.com>
2024-12-23 22:06:22 +0330

    keydown q quit
    @katiko99 made me do this

--- main.cpp
+++ main.cpp
@@ -1,48 +1,44 @@
-//g++ main.cpp -o prog -lSDL2
-#include <iostream>
+// g++ main.cpp -o prog -lSDL2
 #include <SDL2/SDL.h>
-int main(int argc,char* argv[]){
-  //create a window data type
-    SDL_Window* window = nullptr;
+#include <iostream>
+int main(int argc, char *argv[]) {
+  // create a window data type
+  SDL_Window *window = nullptr;
 
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    std ::cout << "could not be initialize !" << SDL_GetError();
+  } else {
+    std::cout << "SDL is working\n";
+  }
 
+  window =
+      SDL_CreateWindow("an SDL2 window", SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+  if (window == NULL) {
+    std ::cout << "could not create window";
+  }
 
-    if(SDL_Init(SDL_INIT_VIDEO)< 0){
-        std :: cout << "could not be initialize !"  << SDL_GetError();
+  // event loop
+  bool GameIsRunning = 1;
+  while (GameIsRunning) {
+    SDL_Event event;
+    // start our event loop
+
+    while (SDL_PollEvent(&event)) {
+      // handel each specific event
+      if (event.type == SDL_QUIT) {
+        GameIsRunning = 0;
+      }
+      // quit support with q
+      if (event.type == SDL_KEYDOWN) {
+        SDL_Keycode key = event.key.keysym.sym;
+        if (key == SDLK_q)
+          GameIsRunning = 0;
+      }
     }
-    else {
-        std::cout << "SDL is working\n";
-    }
+  }
 
-    window = SDL_CreateWindow(
-          "an SDL2 window",
-          SDL_WINDOWPOS_UNDEFINED,
-          SDL_WINDOWPOS_UNDEFINED,
-        640,
-        480,
-        SDL_WINDOW_SHOWN
-        );
-    if (window == NULL){
-        std :: cout << "could not create window" ;
-    }
-
-    //event loop
-    bool GameIsRunning = 1;
-    while(GameIsRunning){
-        SDL_Event event;
-        //start our event loop
-    
-        while(SDL_PollEvent(&event)){
-        //handel each specific event
-        if(event.type == SDL_QUIT){
-           GameIsRunning = 0;
-        }
-        }
-    }
-
-
-    //SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-    return 0;
+  // SDL_Delay(3000);
+  SDL_DestroyWindow(window);
+  return 0;
 }
-
