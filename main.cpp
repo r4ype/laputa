33a13634ba6d159dcb2d1bc5cc65b69bd70b4901
r4ype <raypemaster@gmail.com>
2024-12-26 18:41:55 +0330

    made the player an object

--- main.cpp
+++ main.cpp
@@ -30,9 +30,19 @@ int main(int argc, char *argv[]) {
     std::cout << "Current GPU: " << info.name << std::endl;
   }
 
-  int player_x = 0, player_y = 0;
-  int player_xvel = 0, player_yvel = 0;
-  int PLAYER_SPEED = 4;
+  class Character {
+  public:
+    int x, y;
+    int xvel, yvel;
+    int speed;
+  };
+
+  Character pazu;
+  pazu.x = 0;
+  pazu.y = 0;
+  pazu.xvel = 0;
+  pazu.yvel = 0;
+  pazu.speed = 3;
   // event loop
   bool GameIsRunning = true;
   while (GameIsRunning) {
@@ -49,16 +59,16 @@ int main(int argc, char *argv[]) {
       case SDL_KEYDOWN:
         switch (event.key.keysym.sym) {
         case SDLK_a:
-          player_xvel = -PLAYER_SPEED;
+          pazu.xvel = -pazu.speed;
           break;
         case SDLK_d:
-          player_xvel = PLAYER_SPEED;
+          pazu.xvel = pazu.speed;
           break;
         case SDLK_w:
-          player_yvel = -PLAYER_SPEED;
+          pazu.yvel = -pazu.speed;
           break;
         case SDLK_s:
-          player_yvel = PLAYER_SPEED;
+          pazu.yvel = pazu.speed;
           break;
         case SDLK_q:
           GameIsRunning = false;
@@ -68,35 +78,35 @@ int main(int argc, char *argv[]) {
       case SDL_KEYUP:
         switch (event.key.keysym.sym) {
         case SDLK_a:
-          if (player_xvel < 0)
-            player_xvel = 0;
+          if (pazu.xvel < 0)
+            pazu.xvel = 0;
           break;
         case SDLK_d:
-          if (player_xvel > 0)
-            player_xvel = 0;
+          if (pazu.xvel > 0)
+            pazu.xvel = 0;
           break;
         case SDLK_w:
-          if (player_yvel < 0)
-            player_yvel = 0;
+          if (pazu.yvel < 0)
+            pazu.yvel = 0;
           break;
         case SDLK_s:
-          if (player_yvel > 0)
-            player_yvel = 0;
+          if (pazu.yvel > 0)
+            pazu.yvel = 0;
           break;
         }
         break;
       }
     }
     // update players possition
-    player_x += player_xvel;
-    player_y += player_yvel;
+    pazu.x += pazu.xvel;
+    pazu.y += pazu.yvel;
     // draw the player
     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
     SDL_RenderClear(renderer);
 
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-    SDL_Rect player = {player_x, player_y, 50, 50};
-    SDL_RenderFillRect(renderer, &player);
+    SDL_Rect shape = {pazu.x, pazu.y, 50, 50};
+    SDL_RenderFillRect(renderer, &shape);
 
     SDL_RenderPresent(renderer);
 
