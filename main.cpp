d39dc7f027f7f51c35c28d87c1bb6569bdf307f9
r4ype <raypemaster@gmail.com>
2024-12-30 22:44:46 +0330

    lazygit test

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 #include <SDL2/SDL.h>
+
 #include <iostream>
 
 struct position {
-    int x,y,xvel,yvel;
+    int x, y, xvel, yvel;
 };
 
 class Character {
-    private :
-        position p1 = {0};
-    public:
+   private:
+    position p1 = {0};
 
+   public:
     void init(int startX, int startY, int velocityX, int velocityY) {
         p1.x = startX;
         p1.y = startY;
@@ -22,65 +23,67 @@ class Character {
     position GetCharacterPosition();
 };
 
-void Character::MoveCharacter(char direction){
+void Character::MoveCharacter(char direction) {
     switch (direction) {
-        case 'l' :
+        case 'l':
             p1.x -= p1.xvel;
             break;
-        case 'r' :
+        case 'r':
             p1.x += p1.xvel;
             break;
-        case 'u' :
+        case 'u':
             p1.y -= p1.yvel;
             break;
-        case 'd' :
+        case 'd':
             p1.y += p1.yvel;
             break;
     }
 }
 
-void Character::SetCharacterSpeed(position player){
+void Character::SetCharacterSpeed(position player) {
     p1.xvel = player.xvel;
     p1.yvel = player.yvel;
 }
 
-void Character::SetCharacterPosition(position player){
-    if (p1.x + player.x > 0 && p1.x + player.x < 640){
+void Character::SetCharacterPosition(position player) {
+    if (p1.x + player.x > 0 && p1.x + player.x < 640) {
         p1.x += player.x;
     }
-    if (p1.y + player.y > 0 && p1.y + player.y < 480){
+    if (p1.y + player.y > 0 && p1.y + player.y < 480) {
         p1.y += player.y;
     }
 }
 
-position Character::GetCharacterPosition(){
-    return p1;
-}
+position Character::GetCharacterPosition() { return p1; }
 
 int main(int argc, char* argv[]) {
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        std::cerr << "SDL initialization failed: " << SDL_GetError()
+                  << std::endl;
         return -1;
     }
 
-    SDL_Window* window = SDL_CreateWindow("SDL2 Game", SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    SDL_Window* window =
+        SDL_CreateWindow("SDL2 Game", SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
     if (!window) {
         std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer =
+        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
+        std::cerr << "Failed to create renderer: " << SDL_GetError()
+                  << std::endl;
         SDL_DestroyWindow(window);
         SDL_Quit();
         return -1;
     }
 
     Character pazu;
-    pazu.init(0, 0, 2, 2); // Changed initial velocities to 2 for movement
+    pazu.init(0, 0, 2, 2);  // Changed initial velocities to 2 for movement
     position Now;
 
     bool gameIsRunning = true;
@@ -132,4 +135,4 @@ int main(int argc, char* argv[]) {
     SDL_Quit();
     return 0;
 }
-
+// lazygit test
