854fb2c1b7bc395e023658b76aac1657397a46ba
ehsan <ehsanekarami20@gmail.com>
2025-01-03 12:09:45 +0330

    player collison added and player movement fixed by frame rate and frame rate fixed on 60

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SDL2/SDL_image.h>
 
 #include <iostream>
+#include <algorithm>
 
 #define windowSizeWidth 640
 #define windowSizeHeight 480
@@ -11,41 +12,63 @@ const int width = windowSizeWidth - (windowSizeWidth % gridSize);
 const int height = windowSizeHeight - (windowSizeHeight % gridSize);
 const int CELL_WIDTH = width / gridSize;
 const int CELL_HEIGHT = height / gridSize;
+int board[gridSize][gridSize];
 
 struct position {
     int x, y, xvel, yvel;
 };
+
+enum Keys { LEFT = 0, RIGHT, UP, DOWN };
+
 /////////////////////////////////////////////////////////
 class Character {
    private:
     position p1 = {0};
+    int movementDelay = 5;  // Number of frames to wait between movements
+    int frameCounter = 0;   // Counter to track frames
 
    public:
     void init(int startX, int startY, int velocityX, int velocityY) {
+        board[startX][startY] = 1;
         p1.x = startX;
         p1.y = startY;
         p1.xvel = velocityX;
         p1.yvel = velocityY;
     }
-    void SetCharacterSpeed(position player);
-    position GetCharacterPosition();
     void UpdatePosition(bool keys[4]);
+    position GetCharacterPosition();
 };
 
-void Character::SetCharacterSpeed(position player) {
-    p1.xvel = player.xvel;
-    p1.yvel = player.yvel;
-}
-
-position Character::GetCharacterPosition() { return p1; }
-
 void Character::UpdatePosition(bool keys[4]) {
-    if (keys[0] && p1.x - CELL_WIDTH >= 0) p1.x -= CELL_WIDTH;        // left
-    if (keys[1] && p1.x + CELL_WIDTH < width) p1.x += CELL_WIDTH;     // right
-    if (keys[2] && p1.y - CELL_HEIGHT >= 0) p1.y -= CELL_HEIGHT;      // up
-    if (keys[3] && p1.y + CELL_HEIGHT < height) p1.y += CELL_HEIGHT;  // down
+    frameCounter++;
+    if (frameCounter < movementDelay) {
+        return;  // Skip update if the delay hasn't passed
+    }
+    frameCounter = 0;  // Reset the counter after movement
+
+    if (keys[LEFT] && p1.x - 1 >= 0 && board[p1.x - 1][p1.y] == 0) {
+        std::swap(board[p1.x][p1.y], board[p1.x - 1][p1.y]);
+        p1.x -= 1;
+    }
+    if (keys[RIGHT] && p1.x + 1 < gridSize && board[p1.x + 1][p1.y] == 0) {
+        std::swap(board[p1.x][p1.y], board[p1.x + 1][p1.y]);
+        p1.x += 1;
+    }
+    if (keys[UP] && p1.y - 1 >= 0 && board[p1.x][p1.y - 1] == 0) {
+        std::swap(board[p1.x][p1.y], board[p1.x][p1.y - 1]);
+        p1.y -= 1;
+    }
+    if (keys[DOWN] && p1.y + 1 < gridSize && board[p1.x][p1.y + 1] == 0) {
+        std::swap(board[p1.x][p1.y], board[p1.x][p1.y + 1]);
+        p1.y += 1;
+    }
 }
-//////////////////////////////////////////////////
+
+position Character::GetCharacterPosition() {
+    return {p1.x * CELL_WIDTH, p1.y * CELL_HEIGHT, p1.xvel, p1.yvel};
+}
+
+/////////////////////////////////////////////////////////
 class Window {
    private:
     int width, height;
@@ -55,28 +78,52 @@ class Window {
         width = widthScreen;
         height = heightScreen;
     }
-    void DrawGrid(SDL_Renderer*);
+    void DrawGrid(SDL_Renderer* renderer);
 };
 
 void Window::DrawGrid(SDL_Renderer* renderer) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     for (int i = 0; i < width; i += CELL_WIDTH) {
         SDL_RenderDrawLine(renderer, i, 0, i, height);
     }
     for (int i = 0; i < height; i += CELL_HEIGHT) {
         SDL_RenderDrawLine(renderer, 0, i, width, i);
     }
-    SDL_RenderDrawLine(renderer, width - 1, 0, width - 1, height);
-    SDL_RenderDrawLine(renderer, 0, height - 1, width, height - 1);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    for (int y = 0; y < gridSize; ++y) {
+        for (int x = 0; x < gridSize; ++x) {
+            if (board[x][y] == 1) {  // Draw active cells
+                SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+                SDL_Rect cell = {x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT};
+                SDL_RenderFillRect(renderer, &cell);
+            }
+            if (board[x][y] == 2) {
+                SDL_SetRenderDrawColor(renderer, 0, 255, 0 , 0);
+                SDL_Rect cell = {x * CELL_WIDTH, y * CELL_HEIGHT, CELL_WIDTH, CELL_HEIGHT};
+                SDL_RenderFillRect(renderer, &cell);
+            }
+        }
+    }
 }
-/////////////////////////////////////////////////
+
+/////////////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
-    Window MainWindow;
-    MainWindow.init(width, height);
+    std::fill(&board[0][0], &board[0][0] + sizeof(board) / sizeof(int), 0);
+
+    //draw wall for now should be in window class !!!
+    for(int i = 1 ; i < gridSize-2;i += 2)
+        for(int j = 1 ; j < gridSize-1;j += 2)
+            board[i][j] = 2;
+
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        std::cerr << "SDL initialization failed: " << SDL_GetError()
-                  << std::endl;
+        std::cerr << "SDL initialization failed: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    if (IMG_Init(IMG_INIT_PNG) == 0) {
+        std::cerr << "SDL_image initialization failed: " << IMG_GetError() << std::endl;
+        SDL_Quit();
         return -1;
     }
 
@@ -90,11 +137,9 @@ int main(int argc, char* argv[]) {
         return -1;
     }
 
-    SDL_Renderer* renderer =
-        SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (!renderer) {
-        std::cerr << "Failed to create renderer: " << SDL_GetError()
-                  << std::endl;
+        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
         SDL_DestroyWindow(window);
         IMG_Quit();
         SDL_Quit();
@@ -102,16 +147,31 @@ int main(int argc, char* argv[]) {
     }
 
     SDL_Texture* playerTexture = IMG_LoadTexture(renderer, "assets/player.png");
+    if (!playerTexture) {
+        std::cerr << "Failed to load texture: " << IMG_GetError() << std::endl;
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        IMG_Quit();
+        SDL_Quit();
+        return -1;
+    }
 
     Character pazu;
     pazu.init(0, 0, CELL_WIDTH, CELL_HEIGHT);
     position Now;
 
+    Window MainWindow;
+    MainWindow.init(width, height);
+
     bool gameIsRunning = true;
     SDL_Event event;
-    bool keys[4] = {false, false, false, false};  // left, right, up, down
+    bool keys[4] = {false, false, false, false};
+
+    Uint32 frameStart, frameTime;
 
     while (gameIsRunning) {
+        frameStart = SDL_GetTicks();
+
         while (SDL_PollEvent(&event)) {
             switch (event.type) {
                 case SDL_QUIT:
@@ -120,16 +180,16 @@ int main(int argc, char* argv[]) {
                 case SDL_KEYDOWN:
                     switch (event.key.keysym.sym) {
                         case SDLK_a:
-                            keys[0] = true;
+                            keys[LEFT] = true;
                             break;
                         case SDLK_d:
-                            keys[1] = true;
+                            keys[RIGHT] = true;
                             break;
                         case SDLK_w:
-                            keys[2] = true;
+                            keys[UP] = true;
                             break;
                         case SDLK_s:
-                            keys[3] = true;
+                            keys[DOWN] = true;
                             break;
                         case SDLK_q:
                             gameIsRunning = false;
@@ -139,16 +199,16 @@ int main(int argc, char* argv[]) {
                 case SDL_KEYUP:
                     switch (event.key.keysym.sym) {
                         case SDLK_a:
-                            keys[0] = false;
+                            keys[LEFT] = false;
                             break;
                         case SDLK_d:
-                            keys[1] = false;
+                            keys[RIGHT] = false;
                             break;
                         case SDLK_w:
-                            keys[2] = false;
+                            keys[UP] = false;
                             break;
                         case SDLK_s:
-                            keys[3] = false;
+                            keys[DOWN] = false;
                             break;
                     }
                     break;
@@ -167,7 +227,8 @@ int main(int argc, char* argv[]) {
 
         SDL_RenderPresent(renderer);
 
-        SDL_Delay(100);
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameTime < 16) SDL_Delay(16 - frameTime);  // Cap at ~60 FPS
     }
 
     SDL_DestroyTexture(playerTexture);
@@ -177,3 +238,4 @@ int main(int argc, char* argv[]) {
     SDL_Quit();
     return 0;
 }
+
